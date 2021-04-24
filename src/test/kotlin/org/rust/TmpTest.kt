d5862564f621823572d54d891f1081067b029e9a
Pavel Karateev <lancelote.du.lac@gmail.com>
2021-04-24 20:45:47 +0300

    Dummy test

---
+++ src/test/kotlin/org/rust/TmpTest.kt
@@ -0,0 +1,19 @@
+/*
+ * Use of this source code is governed by the MIT license that can be
+ * found in the LICENSE file.
+ */
+
+package org.rust
+
+import org.junit.Assert
+import org.junit.Test
+import kotlin.random.Random
+
+
+class TmpTest {
+
+    @Test
+    fun test() {
+        Assert.assertNotEquals(1, 2)
+    }
+}
